1a308e66fa6909a15100201a1dd0f0c93b7a3e06
Kathleen Jogno <jognokath@gmail.com>
2026-08-30 09:11:44 +0000

    terraform variables

--- 01-docker-terraform/terraform-demo/main.tf
+++ 01-docker-terraform/terraform-demo/main.tf
@@ -8,13 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project = "terraform-demo-507008"
-  region  = "us-central1"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "terraform-demo-507008-terra-bucket"
-  location      = "US"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -25,4 +26,10 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
